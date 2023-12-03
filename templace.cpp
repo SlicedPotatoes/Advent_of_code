aa64475b07ac89c091f7f8c479dec038b5538396
SlicedPotatoes <53843746+SlicedPotatoes@users.noreply.github.com>
2023-12-03 15:10:08 +0100

    .

--- templace.cpp
+++ templace.cpp
@@ -1,10 +1,13 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
 
-#include <MeasureExecutionTime.cpp>
+#include <Utils/MeasureExecutionTime.cpp>
 
 using namespace std;
+
+vector<string> input;
 
 int parseInput()
 {
@@ -14,15 +17,29 @@ int parseInput()
 
     if (!file.is_open())
     {
-        cerr << "Erreur : impossible d'ouvrir le fichier " << filename;
+        cerr << "Erreur : impossible d'ouvrir le fichier " << filename << '\n';
         return 1;
     }
     string line;
     while (getline(file, line))
     {
+        input.push_back(line);
     }
     file.close();
     return 0;
+}
+
+int processPart1()
+{
+    int codeSize = 0;
+    int memorySize = 0;
+
+    for (string s : input)
+    {
+        codeSize += input.size();
+    }
+
+    cout << codeSize << " " << memorySize << '\n';
 }
 
 int main()
@@ -31,6 +48,8 @@ int main()
     {
         return 1;
     }
+
+    processPart1();
 
     return 0;
 }
