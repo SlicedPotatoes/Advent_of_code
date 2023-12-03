aabc096bd4121196d96158dab9f9c1668f647b89
SlicedPotatoes <53843746+SlicedPotatoes@users.noreply.github.com>
2023-12-03 15:12:16 +0100

    Update template

--- templace.cpp
+++ templace.cpp
@@ -1,13 +1,10 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <vector>
 
 #include <Utils/MeasureExecutionTime.cpp>
 
 using namespace std;
-
-vector<string> input;
 
 int parseInput()
 {
@@ -17,29 +14,15 @@ int parseInput()
 
     if (!file.is_open())
     {
-        cerr << "Erreur : impossible d'ouvrir le fichier " << filename << '\n';
+        cerr << "Erreur : impossible d'ouvrir le fichier " << filename;
         return 1;
     }
     string line;
     while (getline(file, line))
     {
-        input.push_back(line);
     }
     file.close();
     return 0;
-}
-
-int processPart1()
-{
-    int codeSize = 0;
-    int memorySize = 0;
-
-    for (string s : input)
-    {
-        codeSize += input.size();
-    }
-
-    cout << codeSize << " " << memorySize << '\n';
 }
 
 int main()
@@ -48,8 +31,6 @@ int main()
     {
         return 1;
     }
-
-    processPart1();
 
     return 0;
 }
